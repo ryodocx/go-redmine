249b798eda472ea4f1e209322886beb704b650a7
ryodocx <email@ryodocx.net>
2022-07-20 12:44:45 +0900

    disable omitempty

--- api_issue.go
+++ api_issue.go
@@ -12,23 +12,23 @@ type Issue struct {
 	Status              IdName        `json:"status"`
 	Priority            IdName        `json:"priority"`
 	Author              IdName        `json:"author"`
-	AssignedTo          *IdName       `json:"assigned_to,omitempty"`
-	FixedVersion        *IdName       `json:"fixed_version,omitempty"`
-	Parent              *IdName       `json:"parent,omitempty"`
+	AssignedTo          *IdName       `json:"assigned_to"`
+	FixedVersion        *IdName       `json:"fixed_version"`
+	Parent              *IdName       `json:"parent"`
 	Subject             string        `json:"subject"`
 	Description         string        `json:"description"`
-	StartDate           *string       `json:"start_date,omitempty"`
-	DueDate             *string       `json:"due_date,omitempty"`
+	StartDate           *string       `json:"start_date"`
+	DueDate             *string       `json:"due_date"`
 	DoneRatio           int           `json:"done_ratio"`
 	IsPrivate           bool          `json:"is_private"`
-	EstimatedHours      *float64      `json:"estimated_hours,omitempty"`
-	TotalEstimatedHours *float64      `json:"total_estimated_hours,omitempty"`
+	EstimatedHours      *float64      `json:"estimated_hours"`
+	TotalEstimatedHours *float64      `json:"total_estimated_hours"`
 	SpentHours          float64       `json:"spent_hours"`
 	TotalSpentHours     float64       `json:"total_spent_hours"`
 	CustomFields        []CustomField `json:"custom_fields"`
 	CreatedOn           string        `json:"created_on"`
 	UpdatedOn           string        `json:"updated_on"`
-	ClosedOn            *string       `json:"closed_on,omitempty"`
+	ClosedOn            *string       `json:"closed_on"`
 }
 
 func (c *Client) GetIssueByID(id int, opts ...ReqOption) (*Issue, error) {
